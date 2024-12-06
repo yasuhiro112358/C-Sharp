8de44b0c8bec6384dde04ff5d59f5bdd6e0cc427
Yasuhiro WATANABE <info@nabeyasu.com>
2024-12-06 15:50:45 +0900

    Refactor Program.cs to implement greeting methods and update Main for demonstration

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void SayHi()
+    {
+        Console.WriteLine("Hi!");
+    }
+
+    static string GetHi()
+    {
+        return "Hi!";
+    }
+
+    static string GetHi2() => "Hi!";
+
+    static void Main()
+    {
+        SayHi();
+        Console.WriteLine(GetHi());
+        Console.WriteLine(GetHi2());
+    }
+}
