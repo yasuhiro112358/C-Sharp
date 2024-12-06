329f9abe98725030833b321c1a75fdc4dfe9bbd2
Yasuhiro WATANABE <info@nabeyasu.com>
2024-12-06 11:48:23 +0900

    Refactor Program.cs to encapsulate Main method within MyApp class

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class MyApp {
+    static void Main() {
+        Console.WriteLine("Hello, World!");
+    }
+}
