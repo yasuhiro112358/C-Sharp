e9846fc87f109fe706dc00fb9dbea8163731669d
Yasuhiro WATANABE <info@nabeyasu.com>
2024-12-17 15:37:24 +0900

    Implement delegate example with anonymous methods and lambda expressions in MyApp31

--- MyApp31/Program.cs
+++ MyApp31/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+delegate void MyDelegate();
+
+class MyApp31
+{
+    static void Main()
+    {
+        MyDelegate ShowMessage;
+
+        // 匿名メソッド
+        ShowMessage = delegate
+        {
+            Console.WriteLine("Hi!");
+        };
+
+        // ラムダ式: 引数 => 処理
+        // ShowMessage += () =>
+        // {
+        //     Console.WriteLine("Hello!");
+        // };
+        ShowMessage += () => Console.WriteLine("Hello!");
+
+        ShowMessage();
+    }
+}
